3abfedc023a9247957ad6b85e0915e76aaeecff2
Akash-m-SE <72149737+Akash-m-SE@users.noreply.github.com>
2024-05-11 19:57:25 +0530

    updated the page.tsx file for testing purposes

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/listings/ListingCard";
 
-
 interface HomeProps {
   searchParams: IListingsParams;
 }
